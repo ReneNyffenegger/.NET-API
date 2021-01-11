207e9be4667c179b24eae1a117235359fe1507f5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-01-11 09:13:02 +0100

    + writeClobToFile.ps1

---
+++ Oracle/DataAccess/Types/OracleClob/writeClobToFile.ps1
@@ -0,0 +1,29 @@
+add-type -path "$env:oracle_home\odp.net\bin\4\Oracle.DataAccess.dll"
+
+$enc = [System.Text.Encoding]::GetEncoding(1252)
+
+$conn = [Oracle.DataAccess.Client.OracleConnection]::new("User ID=rene/rene;data source=ora19")
+$conn.Open()
+
+$sql  = [Oracle.DataAccess.Client.OracleCommand]::new("select filename, text from tab", $conn)
+$sql.CommandType = [System.Data.CommandType]::Text
+
+$rdr = $sql.ExecuteReader()
+
+while ($rdr.Read()) {
+
+    $filename = $rdr.GetString(0);
+    $text     = $rdr.GetOracleClob(1)
+
+    $dirname  = split-path $filename
+
+    if (-not (test-path $dirname)) {
+       $null = mkdir $dirname
+    }
+
+    $filename
+
+    $out_file = new-object System.IO.StreamWriter "$pwd/$filename", $false, $enc
+    $out_file.Write($text.Value)
+    $out_file.Close()
+}
