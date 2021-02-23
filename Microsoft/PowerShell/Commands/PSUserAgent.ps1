dbc5acca9b25e30ae262bbd0b8a59ac3f7e7de52
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-02-23 10:56:28 +0100

    + Microsoft.PowerShell.Commands.PSUserAgent

---
+++ Microsoft/PowerShell/Commands/PSUserAgent.ps1
@@ -0,0 +1,5 @@
+write-host "Chrome:           $([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome          )"
+write-host "FireFox:          $([Microsoft.PowerShell.Commands.PSUserAgent]::FireFox         )"
+write-host "InternetExplorer: $([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)"
+write-host "Opera:            $([Microsoft.PowerShell.Commands.PSUserAgent]::Opera           )"
+write-host "Safari:           $([Microsoft.PowerShell.Commands.PSUserAgent]::Safari          )"
