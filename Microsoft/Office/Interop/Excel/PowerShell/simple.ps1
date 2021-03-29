b4799ea1ba763d01bf1b599478cd7ba1958ac296
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-03-29 15:48:55 +0200

    + simple PowerShell for Excel Interop

---
+++ Microsoft/Office/Interop/Excel/PowerShell/simple.ps1
@@ -0,0 +1,14 @@
+add-type -path 'C:\Program Files (x86)\Microsoft Office\Office16\DCF\Microsoft.Office.Interop.Excel.dll'
+$xls = new-object Microsoft.Office.Interop.Excel.ApplicationClass
+
+$xls.Visible = $true
+
+$wb = $xls.Workbooks.Add()
+$sh = $wb.Worksheets(1)
+
+$sh.Name = "foo bar baz"
+
+$sh.Cells(1,1) = "Hello world"
+
+$wb.SaveAs("$env:temp\excel-test.xlsx")
+$xls.Quit()
