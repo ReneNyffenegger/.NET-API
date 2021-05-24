534b76bc4c0655909073423695a4288eded8d4ba
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-05-24 10:21:17 +0200

    Remove comment

--- Oracle/DataAccess/Client/_example/PowerShell/call-function.ps1
+++ Oracle/DataAccess/Client/_example/PowerShell/call-function.ps1
@@ -8,7 +8,6 @@ $rnd_stmt.CommandText = 'dbms_random.value'
 #    (see https://support.oracle.com/knowledge/Oracle%20Database%20Products/261084_1.html)
 #
 $par_ret  = $rnd_stmt.parameters.add('ret'   ,  [Oracle.DataAccess.Client.OracleDbType]::Decimal, [System.Data.ParameterDirection]::ReturnValue)
-# $par_ret.Size = 22
 $par_low  = $rnd_stmt.parameters.add('LOW'   ,  [Oracle.DataAccess.Client.OracleDbType]::Decimal, [System.Data.ParameterDirection]::Input      )
 $par_high = $rnd_stmt.parameters.add('HIGH'  ,  [Oracle.DataAccess.Client.OracleDbType]::Decimal, [System.Data.ParameterDirection]::Input      )
 
