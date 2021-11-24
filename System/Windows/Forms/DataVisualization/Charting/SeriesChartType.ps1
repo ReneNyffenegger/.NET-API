1c4f328bdcc57524e0f5c8547c48af62a8fb8476
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-11-24 20:00:02 +0100

    + System.Windows.Forms.DataVisualization.Charting.SeriesChartType

---
+++ System/Windows/Forms/DataVisualization/Charting/SeriesChartType.ps1
@@ -0,0 +1,5 @@
+[enum]::GetValues([System.Windows.Forms.DataVisualization.Charting.SeriesChartType]) | sort-object {
+   $_.ToString()
+} | foreach-object {
+  '{0,-17} {1,-2}' -f $_.ToString(), $_.value__
+}
