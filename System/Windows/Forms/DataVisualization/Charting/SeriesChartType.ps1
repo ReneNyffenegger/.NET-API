76680bd5e5633647bee62b8efa7d7f0e3f27084a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-11-24 20:00:56 +0100

    alignment

--- System/Windows/Forms/DataVisualization/Charting/SeriesChartType.ps1
+++ System/Windows/Forms/DataVisualization/Charting/SeriesChartType.ps1
@@ -1,5 +1,5 @@
 [enum]::GetValues([System.Windows.Forms.DataVisualization.Charting.SeriesChartType]) | sort-object {
    $_.ToString()
 } | foreach-object {
-  '{0,-17} {1,-2}' -f $_.ToString(), $_.value__
+  '{0,-17} {1,2}' -f $_.ToString(), $_.value__
 }
