62e10fe42c7f1ba9bee535a8eba86564ee1f389e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2025-02-18 19:59:16 +0100

    remove ExecuteNonQuery where return value is not checked

--- Oracle/DataAccess/Client/_example/PowerShell/insert.ps1
+++ Oracle/DataAccess/Client/_example/PowerShell/insert.ps1
@@ -1,6 +1,6 @@
 $ins_stmt = $con.CreateCommand()
 $ins_stmt.CommandText = 'insert into tq84_table(a, b) values (:a, :b)'
-$param_a  = $ins_stmt.Parameters.Add(':a' , [Oracle.DataAccess.Client.OracleDbType]::Decimal)
+$param_a  = $ins_stmt.Parameters.Add(':a' , [Oracle.DataAccess.Client.OracleDbType]::Decimal )
 $param_b  = $ins_stmt.Parameters.Add(':b' , [Oracle.DataAccess.Client.OracleDbType]::Varchar2)
 
 $param_a.value = 42
@@ -11,7 +11,6 @@ if ($ins_stmt.ExecuteNonQuery() -ne 1) {
 
 $param_a.value = 99
 $param_b.value ='ninety-nine'
-$ins_stmt.ExecuteNonQuery()
 if ($ins_stmt.ExecuteNonQuery() -ne 1) {
    write-host "Expected: 1"
 }
